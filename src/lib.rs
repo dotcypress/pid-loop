a516c4eb8f687e69386a0df89b0e572c540357b5
Vitaly Domnikov <dotcypress@gmail.com>
2021-11-21 13:19:39 -0800

    0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,75 @@
+//! A tiny `no_std` PID controller library.
+//!
+//! This crate implements the classic PID loop over abstracted data type.
+//!
+//! # Introduction
+//!
+//! A proportional–integral–derivative controller is a control loop mechanism
+//! employing feedback that is widely used in industrial control systems
+//! and a variety of other applications requiring continuously modulated control.
+//!
+//! # Examples
+//!
+//! ```no_run
+//! use pid_loop::PID;
+//!
+//! let target = 42.0;
+//! let mut controller = PID::new(2.0, 0.7, 0.3);
+//! loop {
+//!     let correction = controller.next(target, measure());
+//!     apply_correction(correction);
+//!     sleep();
+//! }
+//!
+//! fn sleep() { todo!() }
+//! fn measure() -> f32 { todo!() }
+//! fn apply_correction(_: f32) { todo!() }
+//! ```
+
 #![no_std]
-#![deny(nonstandard_style)]
-#![warn(future_incompatible, unreachable_pub, rust_2018_idioms)]
+#![deny(nonstandard_style, future_incompatible, rust_2018_idioms)]
 use core::ops::*;
 
-pub struct PID<F: Ord + Copy> {
-    kp: F,
-    ki: F,
-    kd: F,
+/// PID controller
+///
+/// # Examples
+///
+/// ```no_run
+/// use pid_loop::PID;
+///
+/// let target = 42.0;
+/// let mut controller = PID::<f64>::new(2.0, 0.7, 0.3);
+/// let correction = controller.next(target, measure());
+///
+/// fn measure() -> f64 { todo!() }
+/// ```
+pub struct PID<F> {
+    /// Proportional gain.
+    pub kp: F,
+    /// Integral gain.
+    pub ki: F,
+    /// Derivative gain.
+    pub kd: F,
     last_error: F,
     error_sum: F,
-    error_sum_min: Option<F>,
-    error_sum_max: Option<F>,
+    min_error_sum: Option<F>,
+    max_error_sum: Option<F>,
 }
 
 impl<F> PID<F>
 where
-    F: Default + Add<Output = F> + Sub<Output = F> + Mul<Output = F> + Ord + Copy,
+    F: Default + Add<Output = F> + Sub<Output = F> + Mul<Output = F> + PartialOrd + Copy,
 {
+    /// Create a new instance of `PID`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// #![allow(unused_assignments)]
+    /// use pid_loop::PID;
+    ///
+    /// let mut controller = PID::<f32>::new(0.7, 0.034, 0.084);
+    /// ```
     pub fn new(kp: impl Into<F>, ki: impl Into<F>, kd: impl Into<F>) -> Self {
         Self {
             kp: kp.into(),
@@ -24,31 +77,86 @@ where
             kd: kd.into(),
             last_error: F::default(),
             error_sum: F::default(),
-            error_sum_min: None,
-            error_sum_max: None,
+            min_error_sum: None,
+            max_error_sum: None,
         }
     }
 
-    pub fn set_error_sum_min(&mut self, min: Option<F>) {
-        self.error_sum_min = min;
+    /// Reset controller internal state.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// #![allow(unused_assignments)]
+    /// use pid_loop::PID;
+    ///
+    /// let target = 30.0;
+    /// let mut controller = PID::<f32>::new(0.7, 0.034, 0.084);
+    /// controller.next(target, 42.0);
+    /// controller.reset();
+    ///
+    /// ```
+    pub fn reset(&mut self) {
+        self.last_error = F::default();
+        self.error_sum = F::default();
     }
 
-    pub fn set_error_sum_max(&mut self, max: Option<F>) {
-        self.error_sum_max = max;
+    /// Set minimum limit for error sum.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// #![allow(unused_assignments)]
+    /// use pid_loop::PID;
+    ///
+    /// let target = 30;
+    /// let mut controller = PID::<i128>::new(7, 34, 4);
+    /// controller.set_min_error_sum(Some(-300));
+    /// ```
+    pub fn set_min_error_sum(&mut self, min: Option<F>) {
+        self.min_error_sum = min;
     }
 
+    /// Set maximum limit for error sum.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// #![allow(unused_assignments)]
+    /// use pid_loop::PID;
+    ///
+    /// let target = 30.0;
+    /// let mut controller = PID::<f32>::new(0.7, 0.034, 0.084);
+    /// controller.set_max_error_sum(Some(300.0));
+    /// ```
+    pub fn set_max_error_sum(&mut self, max: Option<F>) {
+        self.max_error_sum = max;
+    }
+
+    /// Push next measurement into the controller and return correction.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// #![allow(unused_assignments)]
+    /// use pid_loop::PID;
+    ///
+    /// let target = 30.0;
+    /// let mut controller = PID::<f64>::new(0.7, 0.034, 0.084);
+    /// let correction = controller.next(target, 42.0);
+    /// ```
     pub fn next(&mut self, sp: impl Into<F>, fb: impl Into<F>) -> F {
         let error = sp.into() - fb.into();
         let error_delta = error - self.last_error;
         self.last_error = error;
 
         let error_sum = self.error_sum + error;
-        let error_sum = match &self.error_sum_min {
-            Some(min) if min > &error_sum => *min,
+        let error_sum = match &self.max_error_sum {
+            Some(max) if &error_sum > max => *max,
             _ => error_sum,
         };
-        let error_sum = match &self.error_sum_max {
-            Some(max) if max < &error_sum => *max,
+        let error_sum = match &self.min_error_sum {
+            Some(min) if &error_sum < min => *min,
             _ => error_sum,
         };
         self.error_sum = error_sum;
