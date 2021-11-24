82e9a42988c4c3af3ea351f90606f55de5b0ccde
Vitaly Domnikov <dotcypress@gmail.com>
2021-11-24 10:23:02 -0800

    update tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 //! use pid_loop::PID;
 //!
 //! let target = 42.0;
-//! let mut controller = PID::<f32, 1>::new(2.0, 0.7, 0.3);
+//! let mut controller = PID::<f32, 1>::new(2.0, 0.7, 0.3, 0.0, 0.0);
 //! loop {
 //!     let correction = controller.next(target, measure());
 //!     apply_correction(correction);
@@ -38,7 +38,7 @@ use core::ops::*;
 /// use pid_loop::PID;
 ///
 /// let target = 42.0;
-/// let mut controller = PID::<f64, 1>::new(2.0, 0.7, 0.3);
+/// let mut controller = PID::<f64, 1>::new(2.0, 0.7, 0.3, 0.0, 0.0);
 /// let correction = controller.next(target, measure());
 ///
 /// fn measure() -> f64 { todo!() }
@@ -54,9 +54,9 @@ pub struct PID<F, const W: usize> {
     pub kf: F,
     ///Velocity
     pub kv: F,
+    last_sp: F,
     last_error_idx: usize,
     errors: [F; W],
-    prev_sp: F,
 }
 
 impl<F, const W: usize> PID<F, W>
@@ -73,7 +73,13 @@ where
     ///
     /// let mut controller = PID::<f32, 1>::new(0.7, 0.034, 0.084, 0.1, 0.0);
     /// ```
-    pub fn new(kp: impl Into<F>, ki: impl Into<F>, kd: impl Into<F>, kf: impl Into<F>, kv: impl Into<F>) -> Self {
+    pub fn new(
+        kp: impl Into<F>,
+        ki: impl Into<F>,
+        kd: impl Into<F>,
+        kf: impl Into<F>,
+        kv: impl Into<F>,
+    ) -> Self {
         assert!(W > 0);
         Self {
             kp: kp.into(),
@@ -81,9 +87,9 @@ where
             kd: kd.into(),
             kf: kf.into(),
             kv: kv.into(),
+            last_sp: F::default(),
             errors: [F::default(); W],
             last_error_idx: 0,
-            prev_sp: F::default(),
         }
     }
 
@@ -96,15 +102,15 @@ where
     /// use pid_loop::PID;
     ///
     /// let target = 30.0;
-    /// let mut controller = PID::<f32, 1>::new(0.7, 0.034, 0.084, 0.1. 0.0);
+    /// let mut controller = PID::<f32, 1>::new(0.7, 0.034, 0.084, 0.1, 0.0);
     /// controller.next(target, 42.0);
     /// controller.reset();
     ///
     /// ```
     pub fn reset(&mut self) {
+        self.last_sp = F::default();
         self.last_error_idx = 0;
         self.errors = [F::default(); W];
-        self.prev_sp = F::default();
     }
 
     /// Push next measurement into the controller and return correction.
@@ -120,9 +126,9 @@ where
     /// let correction = controller.next(target, 42.0);
     /// ```
     pub fn next(&mut self, sp: impl Into<F>, fb: impl Into<F>) -> F {
-        let sp_f = sp.into();
-        let fp_f =  fb.into();
-        let error = sp_f - fp_f;
+        let sp = sp.into();
+        let fb = fb.into();
+        let error = sp - fb;
 
         let error_delta = error - self.errors[self.last_error_idx];
         self.last_error_idx += 1;
@@ -132,12 +138,14 @@ where
         self.errors[self.last_error_idx] = error;
         let err_history = self.errors.iter().fold(F::default(), |acc, i| acc + *i);
 
+        let sp_delta = sp - self.last_sp;
+        self.last_sp = sp;
+
         let p = self.kp * error;
         let i = self.ki * err_history;
         let d = self.kd * error_delta;
-        let f = self.kf * (sp_f - self.prev_sp) ;
-        let v = self.kv * fp_f;
-        self.prev_sp = sp_f;
+        let f = self.kf * sp_delta;
+        let v = self.kv * fb;
         p + i + d + f + v
     }
 }
